dcbd88c7c60b47102f74b9b89b541684787e67d1
Juju26 <atshayakumarr26@gmail.com>
2021-07-13 22:45:30 +0530

    13 7 commit

---
+++ cross pattern.py
@@ -0,0 +1,25 @@
+'''
+P         M
+ R      A
+   O  R
+     G
+  O    R
+ R       A
+P          M
+'''
+s='PROGRAM'
+n=len(s)
+c=n-1
+for i in range(0,n):
+    for j in range(0,n):
+        if j==c and i!=j:
+            print(s[c],end='')
+            c-=1
+        if i==j and i!=c:
+            print(s[i],end='')
+        elif i== c==j:
+            print(s[i],end='')
+            c-=1
+        else:
+            print(' ',end='')
+    print()
---
+++ find peak ele.py
@@ -0,0 +1,55 @@
+#class Solution:
+    #def findPeakElement(self, nums: List[int]) -> int:
+nums=[1,2,4,3,5]
+c=0
+l=0
+if len(nums)==2:
+    c=nums.index(max(c,nums[1]))
+    
+elif len(nums)==1:
+    c=nums.index(nums[0])
+    
+else:
+    for i in range(1,len(nums)-1):
+        if  nums[i]>nums[i-1] and nums[i]>nums[i+1]:
+            c=nums.index(nums[i]) 
+            
+        else:
+            print(c)
+            
+print(c)
+
+'''
+Input:
+[6,5,4,3,2,3,2]
+Output:
+3
+Expected:
+0
+
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+Example 2:
+
+Input: nums = [1,2,1,3,5,6,4]
+Output: 5
+Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where t
+
+
+working code: 
+
+n=len(nums)
+        if n==1:
+            return 0
+        elif (nums[0]>=nums[1]):
+            return 0
+        elif (nums[n-1]>=nums[n-2]):
+            return n-1 
+        else:
+            for i in range(1,n-1):
+                if (nums[i]>=nums[i-1] and nums[i]>=nums[i+1]):
+                    return i
+
+'''
