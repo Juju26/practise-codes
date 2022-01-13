67f505d9d4259b840f73d444fd56d16104c0f919
Juju26 <atshayakumarr26@gmail.com>
2022-01-13 09:43:36 +0530

    oop opening

---
+++ oop with python/method_overloading.py
@@ -0,0 +1,26 @@
+# python does'nt support method overloading 
+# we can't have same method name twice in a class 
+# so to implement overloading we declare variables of type none and 
+#         replace its value upon passing paramenter (line 10 : m3=none )
+
+# also we need can check number of parameters by conditions(line no:17)
+
+
+class overload:
+    def __init__(self,m1,m2,m3=None) -> None:
+        self.m1=m1
+        self.m2=m2
+        self.m3=m3
+    
+    
+    def sum(self):
+        if self.m3==None:
+            return self.m1+self.m2
+        else: return self.m1+self.m2+self.m3
+    # def sum(self):
+    #     return self.m1+self.m2
+
+obj1=overload(100,200)
+print(obj1.sum())
+obj2=overload(100,400,500)
+print(obj2.sum())
--- skillrack/subpalin.py
+++ skillrack/subpalin.py
@@ -1,10 +1,11 @@
-s='delegate' #input()
+s='aabaa'
+#s='delegate' #input()
 c=0
 for i in range(len(s)):
     for j in range(i+1,len(s)+1):
         p=s[i:j]
-        print(p)
+        #print(p)
         if p==p[::-1] and len(p)>=2:
-            print("cou :",p)
+            #print("cou :",p)
             c+=1
 print(c)
